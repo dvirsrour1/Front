9bb1c1e73023e7c953b20bec3f3aa29fdb244257
Dvir Srour <dvirsrour1@gmail.com>
2024-09-10 15:24:35 +0300

    changes 10/9/2024 - not working

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Buffer} from "node:buffer";
 import './css_files/Main.css';
-import './MainPopUp';
 import './userPopUp'
 import UserPopUp from "./userPopUp";
 class Main extends Component {
@@ -9,7 +8,6 @@ class Main extends Component {
         return (
             <React.Fragment>
                 <h1 className="Header">Welcome to our HTTP System!</h1>
-                <h2>What would you like to do?</h2>
                 <UserPopUp></UserPopUp>
                 <button className="button"><span>See all Users</span></button>
                 <button className="button"><span>See all Tasks</span></button>
--- src/components/userPopUp.tsx
+++ src/components/userPopUp.tsx
@@ -1,20 +1,23 @@
 import React, {Component, useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import {Button} from "react-bootstrap";
-
-interface Props {
-    isModalOpen: boolean;
-}
+import './css_files/userPopUp.css'
+import userIcon from './user.png'
 interface State{
      isModalOpen: boolean;
+     newUserId: string;
+     newUserName: string;
+     newUserDescription: string;
 }
 class UserPopUp extends Component<{}, State> {
     constructor(props: {}) {
         super(props);
 
         this.state = {
-            isModalOpen: false
-
+            isModalOpen: false,
+            newUserId: "",
+            newUserName: "",
+            newUserDescription: ""
         }
     }
      ChangeState = () => {
@@ -30,6 +33,20 @@ class UserPopUp extends Component<{}, State> {
         }
     }
 
+    PrintAndChangeState = () => {
+        this.setState({
+            isModalOpen: true
+        })
+        alert(`Add new User ${this.state.newUserId}, ${this.state.newUserName}, ${this.state.newUserDescription}`);
+    }
+
+    handleIdChange(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            newUserId: e.target.value
+        })
+    }
+
+
     render() {
         return (
             <React.Fragment>
@@ -39,10 +56,35 @@ class UserPopUp extends Component<{}, State> {
         <Modal show={this.state.isModalOpen} onHide={this.ChangeState}>
             <Modal.Header closeButton></Modal.Header>
             <Modal.Body>
-                <Modal.Title>Hi</Modal.Title>
+                <form>
+                <img src={userIcon} alt="Logo" height='40px' width='40px' className={'img'}/>
+                <div className='div-spaces'></div>
+                <div className='div-spaces'></div>
+                <div className='div'></div>
+                {/*<Modal.Title className={'modal-title'}>New User</Modal.Title>*/}
+                <div className='div-spaces'></div>
+                <h1 className={'text-of-titles'}>User name:</h1>
+                <div className='div-spaces'></div>
+                <input type='text' className={'input-group-text style-adding-to-text'} id='input_of_user_name'></input>
+                <div className='div-spaces'></div>
+                <h1 className={'text-of-titles'}>Id:</h1>
+                <div className='div-spaces'></div>
+                    <input
+                        type='text'
+                        className='input-group-text style-adding-to-text'
+                        id='input_of_user_id'
+                        value={this.state.newUserId}
+                        onChange={this.handleIdChange}
+                    />
+                    <div className='div-spaces'></div>
+                    <h1 className={'text-of-titles'}>Description:</h1>
+                    <div className='div-spaces'></div>
+                    <input type='text' className={'input-group-text style-adding-to-text'} value={this.state.newUserDescription}></input>
+                <div className='div-spaces'></div>
+                </form>
             </Modal.Body>
-            <Button onClick={this.ChangeState}>
-                Close
+            <Button onClick={this.PrintAndChangeState}>
+            Submit
             </Button>
         </Modal>
             </React.Fragment>
